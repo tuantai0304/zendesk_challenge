3796177bb2242ad4360b66b98baa7d56701becb9
tuantai0304 <tuantai0304@gmail.com>
2017-07-21 00:26:15 +1000

    Done. Added unit tests

--- test/index_test.js
+++ test/index_test.js
@@ -1,21 +1,38 @@
 'use strict';
 
-let chai = require ('chai');
-let chaiHttp = require ('chai-http');
-let should = chai.should();
-let server = require('../app');
+var chai = require ('chai');
+var chaiHttp = require ('chai-http');
+var should = chai.should();
+var assert = chai.assert;
+
+
+var server = require('../app');
+
+var config = require('../config');
 
 chai.use(chaiHttp);
 
-describe('/GET', () => {
-    it('View first 25 tickets', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((err, res) => {
-               res.should.have.status(200);
-                if (err) throw err;
-                done();
-            });
-    })
+describe('Helper method', () => {
+   it("Get all tickets", function (done) {
+       config.helper.getTickets(2, function (returnObject) {
+          returnObject.should.have.property('tickets');
+           assert.equal(returnObject.statusCode, 200);
+           done();
+       });
+   });
 
+    it("Get Ticket detail", function (done) {
+        config.helper.getTicketsDetail(2, function (returnObject) {
+            returnObject.should.have.property('ticket');
+            assert.equal(returnObject.statusCode, 200);
+            done();
+        });
+    });
+
+    it("A ticket ID not found", function (done) {
+        config.helper.getTicketsDetail(2000, function (returnObject) {
+            assert.equal(returnObject.statusCode, 404);
+            done();
+        });
+    });
 });
